b7dde946f5697da190a116bb374107fd39667d25
Krestine H <krha0001@stud.kea.dk>
2023-08-24 11:10:04 +0200

    added javascript

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const img = document.querySelector("img");
+const firstchild = document.querySelector("article:first-child");
+const secondchild = document.querySelector("article:nth-child(2)");
